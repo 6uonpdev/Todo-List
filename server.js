872394ad2ba5358949c2131e50a0b617fc4d89ba
ntd1911 <duongc1lhp2015@gmail.com>
2025-12-19 07:27:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const { Pool } = require("pg");
 
 const app = express();
-const PORT = 5432;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
