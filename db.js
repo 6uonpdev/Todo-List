81934433e646af5c8e078dbde248ad300244eb34
ntd1911 <duongc1lhp2015@gmail.com>
2026-01-15 23:21:55 +0700

    add test db route

--- db.js
+++ db.js
@@ -1,15 +1,32 @@
 const { Pool } = require("pg");
 
-const db = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "123456",
-  database: "todo_app",
-  port: 5432
-});
+const isProd = process.env.NODE_ENV === "production";
 
-db.on("connect", () => {
-  console.log("PostgreSQL connected");
-});
+const db = new Pool(
+  isProd
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        host: "localhost",
+        user: "postgres",
+        password: "123456",
+        database: "todo_app",
+        port: 5432
+      }
+);
 
 module.exports = db;
+
+
+//const { Pool } = require("pg");
+
+// const db = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   password: "123456",
+//   database: "todo_app",
+//   port: 5432
+// });
+//cấu hình db cho deploy lên web
